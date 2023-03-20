724fea5f014458cd0403aa354174814472ec1680
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-03-20 18:25:51 +0900

    [FEAT] 메인 로직 추가

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,12 +1,27 @@
 package game;
 
+import game.baseball.Baseball;
+import game.baseball.BaseballGameStrategy;
+import game.baseball.message.BaseballGameMessage;
+import game.baseball.rule.BaseballGameRule;
 import game.baseball.view.InputView;
+import game.baseball.view.ResultView;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+
+		Baseball baseball = Baseball.of("345", BaseballGameRule.BASIC);
+
 		while (true) {
-			String inputString = InputView.input();
+			baseball.start(InputView.input(BaseballGameMessage.INPUT_THE_NUMBER), new BaseballGameStrategy());
+			ResultView.print(baseball);
+
+			if (baseball.isOut()) {
+				if (!baseball.toBeContinue(InputView.input(BaseballGameMessage.CONTINUE))) {
+					break;
+				}
+			}
 		}
 	}
 }
