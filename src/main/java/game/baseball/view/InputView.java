dd157a331128063b56a9cec6aeb5598715c0dca9
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-03-20 18:25:36 +0900

    [FEAT] View 로직 추가

--- src/main/java/game/baseball/view/InputView.java
+++ src/main/java/game/baseball/view/InputView.java
@@ -2,12 +2,14 @@ package game.baseball.view;
 
 import java.util.Scanner;
 
+import game.baseball.message.BaseballGameMessage;
+
 public class InputView {
 
 	private static Scanner scanner = new Scanner(System.in);
 
-	public static String input() {
-		System.out.print("숫자를 입력해주세요 : ");
+	public static String input(BaseballGameMessage baseballGameMessage) {
+		System.out.print(baseballGameMessage.getSentence());
 		return scanner.nextLine();
 	}
 }
---
+++ src/main/java/game/baseball/view/ResultView.java
@@ -0,0 +1,10 @@
+package game.baseball.view;
+
+import game.baseball.Baseball;
+
+public class ResultView {
+
+	public static void print(Baseball baseball) {
+		System.out.println(baseball);
+	}
+}
