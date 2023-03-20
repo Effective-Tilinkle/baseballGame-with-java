1ead73bae1133ac7438915f2deb41ce997c24082
hgkim87 <hgkim8704@gmail.com>
2023-03-21 00:18:53 +0900

    [FEAT] 상수 값, 가져오는 로직 추가

--- src/main/java/game/baseball/contants/BaseballConstants.java
+++ src/main/java/game/baseball/contants/BaseballConstants.java
@@ -1,16 +1,28 @@
 package game.baseball.contants;
 
+import java.util.Arrays;
+
 public enum BaseballConstants {
-	CONTINUE("1"),
-	EXIT("2");
+    CONTINUE("1"),
+    EXIT("2");
 
-	String value;
+    String value;
 
-	BaseballConstants(String value) {
-		this.value = value;
-	}
+    BaseballConstants(String value) {
+        this.value = value;
+    }
 
-	public boolean is(String value) {
-		return this.value.equals(value);
-	}
+    public boolean is(String value) {
+        return this.value.equals(value);
+    }
+
+    public static BaseballConstants getFromValue(String value) {
+
+        return Arrays.stream(values())
+                .filter(baseballConstants -> baseballConstants.is(value))
+                .findFirst()
+                .orElseThrow(() -> {
+                    return new RuntimeException("존재 하지 않는 값");
+                });
+    }
 }
