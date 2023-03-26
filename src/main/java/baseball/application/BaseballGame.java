daf757800f8c4bb4fd95eee00b8dbc21f610440f
JanghoPark <pjhsk113@naver.com>
2023-03-26 17:20:27 +0900

    feat: 비즈니스 로직 작성

--- src/main/java/baseball/application/BaseballGame.java
+++ src/main/java/baseball/application/BaseballGame.java
@@ -1,7 +1,22 @@
 package baseball.application;
 
+import baseball.domain.BallCounter;
+import baseball.domain.GameHost;
+import baseball.domain.Player;
+import baseball.domain.Referee;
+import baseball.utils.BallNumberGenerator;
+import baseball.view.InputView;
+import baseball.view.ResultView;
+
 public class BaseballGame {
     public void play() {
+        GameHost computer = GameHost.from(BallNumberGenerator.generate());
+        Referee referee = Referee.of(computer.getTargetNumbers());
 
+        while(referee.isNotStrikeOut()) {
+            Player player = Player.from(InputView.enterNumber());
+            BallCounter ballCounter = referee.call(player.getPickNumbers());
+            ResultView.printResult(ballCounter);
+        }
     }
 }
