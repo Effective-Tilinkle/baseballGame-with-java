f7cb5e7d53d7088436d2d3ae79949e68467d1c10
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-03-30 15:01:07 +0900

    [FIX] input view 입력 기능과 result view 메서드 시그니처 수정

--- src/main/java/game/baseball/view/InputView.java
+++ src/main/java/game/baseball/view/InputView.java
@@ -6,10 +6,23 @@ import game.baseball.message.BaseballGameMessage;
 
 public class InputView {
 
+	private static final int CONTINUE = 1;
+	private static final int EXIT = 2;
+
 	private static Scanner scanner = new Scanner(System.in);
 
-	public static int input() {
-		System.out.print("숫자를 입력해주세요 : ");
+	public static int inputNo() {
+		System.out.print(BaseballGameMessage.INPUT_THE_baseballNo.getSentence());
 		return Integer.parseInt(scanner.nextLine());
+	}
+
+	public static boolean inputWhetherToContinue() {
+		System.out.println(BaseballGameMessage.CONTINUE.getSentence());
+		int result = Integer.parseInt(scanner.nextLine());
+
+		if (CONTINUE == result) {
+			return true;
+		}
+		return false;
 	}
 }
--- src/main/java/game/baseball/view/ResultView.java
+++ src/main/java/game/baseball/view/ResultView.java
@@ -1,10 +1,25 @@
 package game.baseball.view;
 
-import game.baseball.domain.Baseball;
+import game.baseball.domain.GameResult;
+import game.baseball.message.BaseballGameMessage;
 
 public class ResultView {
 
-	public static void print(Baseball baseball) {
-		System.out.println(baseball);
+	public static void print(GameResult gameResult) {
+
+		StringBuilder message = new StringBuilder();
+		if (gameResult.getStrike() > 0) {
+			message.append(BaseballGameMessage.STRIKE.getSentence(gameResult.getStrike()));
+		}
+		if (gameResult.getStrike() == 3) {
+			message.append(System.lineSeparator()).append(BaseballGameMessage.FIN.getSentence(gameResult.getStrike()));
+		}
+		if (gameResult.getBall() > 0) {
+			message.append(BaseballGameMessage.BALL.getSentence(gameResult.getBall()));
+		}
+		if (gameResult.getStrike() == 0 && gameResult.getBall() == 0) {
+			message.append(BaseballGameMessage.NOTHING.getSentence());
+		}
+		System.out.println(message);
 	}
 }
