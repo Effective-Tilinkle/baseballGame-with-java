1564ed6ecdc252a53fe788e14ad45d9ccd93834a
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-03-30 15:04:47 +0900

    [FIX] 수비 객체가 제어 불가능한 것에 의존적이어서 수정

---
+++ src/main/java/game/baseball/domain/Defender.java
@@ -0,0 +1,23 @@
+package game.baseball.domain;
+
+
+import java.util.List;
+
+import game.baseball.data.BaseballNoGenerator;
+
+public class Defender {
+
+	private Baseball defenceBall;
+
+	private Defender(List<Integer> baseballNos) {
+		this.defenceBall = Baseball.from(baseballNos);
+	}
+
+	public static Defender from(BaseballNoGenerator baseballNoGenerator) {
+		return new Defender((List) baseballNoGenerator.generate());
+	}
+
+	public Baseball getDefenceNos() {
+		return defenceBall;
+	}
+}
