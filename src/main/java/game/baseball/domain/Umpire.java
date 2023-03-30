62176bd03367489491ab1bab9d8af15300df6d8b
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-03-30 15:06:22 +0900

    [FEAT] 심판 객체의 기능 구현

--- src/main/java/game/baseball/domain/Umpire.java
+++ src/main/java/game/baseball/domain/Umpire.java
@@ -2,7 +2,47 @@ package game.baseball.domain;
 
 public class Umpire {
 
-	public void judge(BaseballNos attackNos, BaseballNos defenseNos) {
+	private GameResult gameResult;
 
+	private Umpire() {
 	}
+
+	public static Umpire init() {
+		return new Umpire();
+	}
+
+	public GameResult judge(Baseball attackNos, Baseball defenceNos) {
+
+		gameResult = GameResult.init();
+
+		compareNos(attackNos, defenceNos);
+
+		return gameResult;
+	}
+
+	private void compareNos(Baseball attackNos, Baseball defenceNos) {
+
+		defenceNos.getNos()
+				  .filter(no -> attackNos.contains(no))
+				  .forEach(no -> {
+					  boolean compareResult = comparePosition(defenceNos.getPosition(no), attackNos.getPosition(no));
+					  updateGameStatus(compareResult);
+				  });
+	}
+
+	private boolean comparePosition(int defenceNoPosition, int attackNoPosition) {
+		if (defenceNoPosition == attackNoPosition) {
+			return true;
+		}
+		return false;
+	}
+
+	private void updateGameStatus(boolean compareResult) {
+		if (compareResult) {
+			gameResult.plusStrike();
+			return;
+		}
+		gameResult.plusBall();
+	}
+
 }
