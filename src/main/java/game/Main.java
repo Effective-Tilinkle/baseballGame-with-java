3589e0386a44bd351dfe33487c13a7ad867a9f05
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-03-30 15:09:08 +0900

    [FIX] 야구 게임 서비스 수행 부분 수정

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,22 +1,13 @@
 package game;
 
-import game.baseball.domain.Baseball;
-import game.baseball.message.BaseballGameMessage;
-import game.baseball.rule.BaseballGameRule;
-import game.baseball.strategy.BaseballGameStrategy;
-import game.baseball.view.InputView;
-import game.baseball.view.ResultView;
+import game.baseball.domain.BaseballGame;
+import game.baseball.service.GameService;
 
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
-		Baseball baseball = Baseball.from(BaseballGameRule.BASIC);
-
-		while (baseball.isContinue()) {
-			baseball.start(InputView.input(BaseballGameMessage.INPUT_THE_NUMBER), BaseballGameStrategy.from(baseball));
-			ResultView.print(baseball);
-			baseball.checkStrike();
-		}
+		GameService gameService = GameService.of(BaseballGame.init());
+		gameService.gameStart();
 	}
 }
