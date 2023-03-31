68dbf6d92d27a585aeee137fcdead1c6939134e6
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-03-31 11:57:19 +0900

    [FIX] PR - https://github.com/Effective-Tilinkle/baseballGame-with-java/pull/1#discussion_r1153498648

--- src/test/java/game/baseball/domain/BaseballTest.java
+++ src/test/java/game/baseball/domain/BaseballTest.java
@@ -1,23 +1,33 @@
 package game.baseball.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BaseballTest {
 
 	@DisplayName("야구공 번호의 정상적인 위치번호를 가져오는지 테스트 한다")
-	@Test
-	void getPositionTest() {
-		Baseball baseball = Baseball.from(Arrays.asList(2, 3, 4));
-		assertEquals(0, baseball.getPosition(2));
-		assertEquals(1, baseball.getPosition(3));
-		assertEquals(2, baseball.getPosition(4));
-		assertEquals(-1, baseball.getPosition(5));
+	@ParameterizedTest
+	@MethodSource("positionTestArgumentsProvider")
+	void getPositionTest(List<Integer> input, List<Integer> searchNos, List<Integer> expectedPos) {
+
+		Baseball baseball = Baseball.from(input);
+
+		List<Integer> actualPos = searchNos.stream()
+										   .map(no -> baseball.getPosition(no))
+										   .collect(Collectors.toList());
+
+		assertThat(actualPos).isEqualTo(expectedPos);
 	}
 
 	@DisplayName("중복된 값이 입력되면 예외를 발생 시킨다")
@@ -58,4 +68,14 @@ class BaseballTest {
 				.withMessage("Invalid number contained");
 
 	}
+
+
+	private static Stream<Arguments> positionTestArgumentsProvider() {
+
+		return Stream.of(
+				Arguments.of(Arrays.asList(2, 3, 4), Arrays.asList(2, 3, 4), Arrays.asList(0, 1, 2)),
+				Arguments.of(Arrays.asList(2, 3, 4), Arrays.asList(3, 4, 5), Arrays.asList(1, 2, -1)));
+
+	}
+
 }
